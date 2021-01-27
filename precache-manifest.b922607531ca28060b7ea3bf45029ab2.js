43eebae1fa43d3829b3f876403862a3c168f5a51
prernamalik00 <prernamalik00@gmail.com>
2021-01-28 05:25:28 +0530

    Updates

---
+++ precache-manifest.b922607531ca28060b7ea3bf45029ab2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6458d1e82c5984610c38bac10461c8ec",
+    "url": "/NotesKeeper/index.html"
+  },
+  {
+    "revision": "b71b15da88b1d4ea0295",
+    "url": "/NotesKeeper/static/js/2.177ac34a.chunk.js"
+  },
+  {
+    "revision": "1e483f8e1bc4a900ed26",
+    "url": "/NotesKeeper/static/js/main.c7c628f7.chunk.js"
+  },
+  {
+    "revision": "9b627d10852759b569cc",
+    "url": "/NotesKeeper/static/js/runtime-main.8c5e9a3d.js"
+  }
+]);
